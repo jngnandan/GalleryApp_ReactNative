771335b7098b6eb713ce851967ea2bf4a9df9e75
jngnandan <jngnandan@gmail.com>
2022-10-15 14:32:23 +0100

    images added

--- App.js
+++ App.js
@@ -1,20 +1,114 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+// import { NativeWindStyleSheet } from "nativewind";
+
+
+const imagesList = [
+  {
+    id: 0,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-pond-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-pond-thumbnail-img.png',
+    imageAltText: 'nature mountain with pond',
+    thumbnailAltText: 'nature mountain with pond thumbnail',
+  },
+  {
+    id: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-sunset-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-sunset-thumbnail-img.png',
+    imageAltText: 'nature sunset',
+    thumbnailAltText: 'nature sunset thumbnail',
+  },
+  {
+    id: 2,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-ocean-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-ocean-thumbnail-img.png',
+    imageAltText: 'nature mountain with ocean',
+    thumbnailAltText: 'nature mountain with ocean thumbnail',
+  },
+  {
+    id: 3,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-forest-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-forest-thumbnail-img.png',
+    imageAltText: 'nature mountain with forest',
+    thumbnailAltText: 'nature mountain with forest thumbnail',
+  },
+  {
+    id: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-leaves-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-leaves-thumbnail-img.png',
+    imageAltText: 'nature leaves',
+    thumbnailAltText: 'nature leaves thumbnail',
+  },
+  {
+    id: 5,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-tree-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-tree-thumbnail-img.png',
+    imageAltText: 'nature tree',
+    thumbnailAltText: 'nature tree thumbnail',
+  },
+  {
+    id: 6,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-waterfall-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-waterfall-thumbnail-img.png',
+    imageAltText: 'nature waterfall',
+    thumbnailAltText: 'nature waterfall thumbnail',
+  },
+  {
+    id: 7,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-river-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-river-thumbnail-img.png',
+    imageAltText: 'nature river',
+    thumbnailAltText: 'nature river thumbnail',
+  },
+]
+import {Text, View, Image, FlatList } from 'react-native';
 
 export default function App() {
+  const [activeImageId, setActiveImageId] = useState(imagesList[0].id)
+
+  const renderImageItem = ({item}) => {
+    setActiveImageId(item.id)
+  }
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex flex-col justify-start items-center h-screen px-4 mt-20">
       <StatusBar style="auto" />
+      <Image className="w-full h-1/3 rounded" source={{uri: imagesList[activeImageId].imageUrl}} alt={imagesList[activeImageId].imageAltText} />
+
+      <View>
+        <FlatList
+          data={imagesList}
+          keyExtractor={item => item.id}
+          horizontal
+          // showsHorizontalScrollIndicator={false}
+          renderItem={({item}) => (
+            <Image
+              className="w-10 h-10 m-1 rounded"
+              source={{uri: item.thumbnailUrl}}
+              alt={item.thumbnailAltText}
+              onPress={renderImageItem}
+            />
+          )}
+        />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+// NativeWindStyleSheet.setOutput({
+//   default: "native",
+// });
